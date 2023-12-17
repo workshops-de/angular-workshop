a7017a077d5cde706c1b6e0cf135b2e5df361639
Konstantin Pentarakis <konstantin.pentarakis@logicline.de>
2023-12-17 23:39:00 +0100

    chore: update to Angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = "bookmonkey-client'";
+  title = 'bookmonkey-client';
 }
