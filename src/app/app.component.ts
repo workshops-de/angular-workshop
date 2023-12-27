0fbe192f426ca780c61258de39d879f889393cd2
Konstantin Pentarakis <konstantin.pentarakis@logicline.de>
2023-12-27 22:28:14 +0100

    solve--your-1st-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { BookCardComponent } from './book-card/book-card.component';
 
 @Component({
   selector: 'app-root',
+  imports: [BookCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Angularian!';
-}
+export class AppComponent {}
