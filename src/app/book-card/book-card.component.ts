7da124dc0e2ea5d89ddb3443dd962cb05b100585
Konstantin Pentarakis <konstantin.pentarakis@logicline.de>
2023-12-27 23:56:43 +0100

    solve--apply-an-output-binding

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Book } from '../book';
 
 @Component({
@@ -12,10 +12,13 @@ export class BookCardComponent {
   customStyle = { color: '#064D9E', fontWeight: 600 };
 
   @Input() content!: Book;
+  @Output() detailClick = new EventEmitter<Book>();
 
   handleDetailClick(click: MouseEvent) {
     click.preventDefault();
 
     console.log('Click Details-Link:', click);
+
+    this.detailClick.emit(this.content);
   }
 }
