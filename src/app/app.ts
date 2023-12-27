d489e76ef66acc4b90667a909d51939ab968354a
Konstantin Pentarakis <konstantin.pentarakis@logicline.de>
2023-12-27 23:29:43 +0100

    solve--apply-an-input-binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,4 +6,11 @@ import { BookCard } from './books/book-card/book-card';
   imports: [BookCard],
   templateUrl: './app.html'
 })
-export class App {}
+export class App {
+  book = {
+    title: 'How to win friends',
+    author: 'Dale Carnegie',
+    abstract:
+      "How to Win Friends and Influence People is a self-help book written by Dale Carnegie, published in 1936. Over 30 million copies have been sold worldwide, making it one of the best-selling books of all time. In 2011, it was number 19 on Time Magazine's list of the 100 most influential books."
+  };
+}
