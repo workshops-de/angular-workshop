83df3f0beda0c16ec9c822724b241687e86ca51a
Konstantin Pentarakis <konstantin.pentarakis@logicline.de>
2023-12-27 23:48:24 +0100

    solve--apply-an-event-binding

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -12,4 +12,10 @@ export class BookCardComponent {
   customStyle = { color: '#064D9E', fontWeight: 600 };
 
   @Input() content!: Book;
+
+  handleDetailClick(click: MouseEvent) {
+    click.preventDefault();
+
+    console.log('Click Details-Link:', click);
+  }
 }
