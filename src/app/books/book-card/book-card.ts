9ea503662f0257c766eb3b1bf8dfcc4332276be5
Konstantin Pentarakis <konstantin.pentarakis@logicline.de>
2023-12-27 23:17:36 +0100

    solution--component-property-binding

--- src/app/books/book-card/book-card.ts
+++ src/app/books/book-card/book-card.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.html'
 })
-export class BookCard {}
+export class BookCard {
+  customStyle = signal({ color: '#064D9E', fontWeight: 600 });
+
+  readonly placeholderCover = 'book-cover-placeholder.svg';
+}
