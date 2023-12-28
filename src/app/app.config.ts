7455fd7048c3326e33483d9af334e9dd0ea33d43
Konstantin Pentarakis <konstantin.pentarakis@logicline.de>
2023-12-28 23:11:54 +0100

    solve--load-data-from-local-api

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZonelessChangeDetection()]
+  providers: [provideZonelessChangeDetection(), provideHttpClient()]
 };
