02330de4948b2681d146abce57ba11a02f704de8
Konstantin Pentarakis <konstantin.pentarakis@logicline.de>
2023-12-28 23:11:54 +0100

    solve--load-data-from-local-api

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,26 @@
+import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { render, screen } from '@testing-library/angular';
 import { App } from './app';
+import { Book } from './books/book';
 
 describe('App', () => {
-  it('renders a book card', async () => {
-    await render(App);
+  it('renders a book loaded from the API', async () => {
+    const book: Book = {
+      isbn: '978-3-16-148410-0',
+      cover: '',
+      title: 'How to win friends',
+      author: 'Dale Carnegie',
+      abstract: 'A self-help classic.'
+    };
 
-    expect(screen.getByText('How to win friends')).toBeInTheDocument();
+    await render(App, {
+      providers: [provideHttpClient(), provideHttpClientTesting()]
+    });
+
+    TestBed.inject(HttpTestingController).expectOne('http://localhost:4730/books').flush([book]);
+
+    expect(await screen.findByText('How to win friends')).toBeInTheDocument();
   });
 });
