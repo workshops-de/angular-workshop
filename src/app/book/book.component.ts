9231772155bf14013e1c92aa41893cac573f5c8a
Konstantin Pentarakis <konstantin.pentarakis@logicline.de>
2023-12-29 23:41:27 +0100

    solve--component-lifecycle-basics

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { BookCardComponent } from './book-card/book-card.component';
 import { BookFilterPipe } from './book-filter/book-filter.pipe';
 import { Book } from './book';
 import { BookApiService } from './book-api.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-book',
@@ -10,16 +11,21 @@ import { BookApiService } from './book-api.service';
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss'
 })
-export class BookComponent {
+export class BookComponent implements OnInit, OnDestroy {
   private readonly bookApi = inject(BookApiService);
 
   bookSearchTerm = '';
   books: Book[] = [];
+  bookApiSubscription = Subscription.EMPTY;
 
-  constructor() {
-    this.bookApi
+  ngOnInit(): void {
+    this.bookApiSubscription = this.bookApi
       .getAll()
       .subscribe(booksFromService => (this.books = booksFromService));
+  }
+
+  ngOnDestroy(): void {
+    this.bookApiSubscription.unsubscribe();
   }
 
   goToBookDetails(book: Book) {
