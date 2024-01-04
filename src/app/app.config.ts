c9d9a5d76890c32cc0c6faff1a7796c37aeaf185
Christian Schulz <kontakt@schulzolutions.de>
2024-01-04 21:34:40 +0100

    solve--use-router-with-component-input-bindings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes, withComponentInputBinding())
+  ]
 };
