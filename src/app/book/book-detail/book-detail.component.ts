e5a1d435db9b21b73d0a16b138e811613ddb536f
Christian Schulz <kontakt@schulzolutions.de>
2024-01-04 21:34:40 +0100

    solve--use-router-with-component-input-bindings

--- src/app/book/book-detail/book-detail.component.ts
+++ src/app/book/book-detail/book-detail.component.ts
@@ -1,9 +1,8 @@
-import { Component, inject } from '@angular/core';
-import { Observable, switchMap } from 'rxjs';
-import { Book } from '../book';
-import { ActivatedRoute } from '@angular/router';
-import { BookApiService } from '../book-api.service';
 import { AsyncPipe } from '@angular/common';
+import { Component, effect, inject, input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Book } from '../book';
+import { BookApiService } from '../book-api.service';
 
 @Component({
   selector: 'app-book-detail',
@@ -12,14 +11,16 @@ import { AsyncPipe } from '@angular/common';
   styleUrl: './book-detail.component.scss'
 })
 export class BookDetailComponent {
-  private readonly route = inject(ActivatedRoute);
   private readonly bookApi = inject(BookApiService);
+  book$!: Observable<Book>;
 
-  book$: Observable<Book>;
+  isbn = input.required<string>();
 
   constructor() {
-    this.book$ = this.route.params.pipe(
-      switchMap(params => this.bookApi.getByIsbn(params?.['isbn']))
-    );
+    effect(() => this.getBookByIsbn(this.isbn()));
+  }
+
+  private getBookByIsbn(isbn: string): void {
+    this.book$ = this.bookApi.getByIsbn(isbn);
   }
 }
