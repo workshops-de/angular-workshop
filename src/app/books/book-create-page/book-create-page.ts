1cace849943f29b2e2067c4a4ddb0d25d14f01d6
Christian Schulz <kontakt@schulzolutions.de>
2024-01-04 21:22:55 +0100

    solution--router-guards-canDeactivate

--- src/app/books/book-create-page/book-create-page.ts
+++ src/app/books/book-create-page/book-create-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 import { BookCreateForm } from '../book-create-form/book-create-form';
 
@@ -7,4 +7,9 @@ import { BookCreateForm } from '../book-create-form/book-create-form';
   imports: [BookCreateForm],
   templateUrl: './book-create-page.html'
 })
-export class BookCreatePage {}
+export class BookCreatePage {
+  // Does the embedded form hold unsaved changes? Hard-coded for this exercise so
+  // the guard has something to react to - a later exercise derives it from the
+  // form's own dirty state.
+  readonly hasUnsafeChanges = signal(true);
+}
