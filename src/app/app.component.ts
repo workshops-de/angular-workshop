b5b58cbff0ac40fee223060471305f091ec7554f
Christian Schulz <kontakt@schulzolutions.de>
2024-01-04 14:41:42 +0100

    solve--add-basic-routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { BookComponent } from './book/book.component';
+import { RouterOutlet } from '@angular/router';
+
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [BookComponent],
+  imports: [NavigationComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss'
+})
+export class NavigationComponent {}
