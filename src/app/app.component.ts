e8d1657b2acc86254dc2f7b0d7e31b5ee1bce2e1
Christian Schulz <kontakt@schulzolutions.de>
2024-01-04 14:41:42 +0100

    solve--add-basic-routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { BookComponent } from './book/book.component';
+import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BookComponent],
+  imports: [BookComponent, NavigationComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
