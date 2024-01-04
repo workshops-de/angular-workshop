3dc5b20352a99c6e986af8c8ea3d70bc5648beb4
Christian Schulz <kontakt@schulzolutions.de>
2024-01-04 21:18:41 +0100

    solve--use-lazy-loading-for-book-feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { BookComponent } from './book/book.component';
-import { BookDetailComponent } from './book/book-detail/book-detail.component';
 
 export const routes: Routes = [
   {
@@ -15,10 +13,6 @@ export const routes: Routes = [
   },
   {
     path: 'books',
-    component: BookComponent
-  },
-  {
-    path: 'books/detail/:isbn',
-    component: BookDetailComponent
+    loadChildren: () => import('./book/book.routes').then(mod => mod.bookRoutes)
   }
 ];
