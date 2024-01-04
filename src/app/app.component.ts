895af7d164333677e338f44e139bc1054a7f92a5
Christian Schulz <kontakt@schulzolutions.de>
2024-01-04 21:18:41 +0100

    solve--use-lazy-loading-for-book-feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { BookComponent } from './book/book.component';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [BookComponent, NavigationComponent, RouterOutlet],
+  imports: [NavigationComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
