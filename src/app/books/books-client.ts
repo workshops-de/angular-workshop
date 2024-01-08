f0242e31f9295ad278feb76e7f41f78702f29865
Martina Kraus <kraus.martina.m@gmail.com>
2024-01-08 15:53:43 +0100

    solve--extend-book-api-service

--- src/app/books/books-client.ts
+++ src/app/books/books-client.ts
@@ -3,19 +3,20 @@ import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Book } from './book';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class BooksClient {
   private readonly http = inject(HttpClient);
-
-  readonly #baseUrl = 'http://localhost:4730';
+  private readonly baseUrl = 'http://localhost:4730';
 
   getAll(): Observable<Book[]> {
-    return this.http.get<Book[]>(`${this.#baseUrl}/books`);
+    return this.http.get<Book[]>(`${this.baseUrl}/books`);
   }
 
   getByIsbn(isbn: string): Observable<Book> {
-    return this.http.get<Book>(`${this.#baseUrl}/books/${isbn}`);
+    return this.http.get<Book>(`${this.baseUrl}/books/${isbn}`);
+  }
+
+  create(book: Partial<Book>): Observable<Book> {
+    return this.http.post<Book>('http://localhost:4730/books', book);
   }
 }
