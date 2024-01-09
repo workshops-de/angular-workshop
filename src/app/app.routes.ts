394aa3c3c04518709637af4d80e396099a620805
Christian Schulz <kontakt@schulzolutions.de>
2024-01-09 14:19:36 +0100

    solution--router-guards-canMatch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Welcome } from '@workshop-support';
+import { isUserAuthenticatedGuardFn } from './is-user-authenticated';
 
 export const routes: Routes = [
   {
@@ -9,6 +10,7 @@ export const routes: Routes = [
   },
   {
     path: 'books',
-    loadChildren: () => import('./books/book.routes').then(mod => mod.bookRoutes)
+    loadChildren: () => import('./books/book.routes').then(mod => mod.bookRoutes),
+    canMatch: [isUserAuthenticatedGuardFn]
   }
 ];
