b677a50648cf9ea005558adda2ed8d5940f9742f
Martina Kraus <kraus.martina.m@gmail.com>
2024-03-12 22:22:36 +0100

    solve--add-formarray

--- src/app/book/book-new/book-new.component.ts
+++ src/app/book/book-new/book-new.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { FormArray, FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgForOf, NgIf } from '@angular/common';
 import { BookApiService } from '../book-api.service';
 import { take } from 'rxjs';
 import { validAuthorName } from '../validators/author.validator';
@@ -8,7 +8,7 @@ import { validAuthorName } from '../validators/author.validator';
 @Component({
   selector: 'app-book-new',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [ReactiveFormsModule, NgIf, NgForOf],
   templateUrl: './book-new.component.html',
   styleUrls: ['./book-new.component.scss']
 })
@@ -16,7 +16,7 @@ export class BookNewComponent {
   form = this.formBuilder.nonNullable.group({
     title: ['', [Validators.required]],
     subtitle: [''],
-    author: ['', [Validators.required, validAuthorName()]],
+    authors: this.formBuilder.array([['', [Validators.required, validAuthorName()]]]),
     abstract: [''],
     isbn: ['']
   });
@@ -25,6 +25,21 @@ export class BookNewComponent {
   }
 
   submit() {
-    this.bookApiService.create(this.form.getRawValue()).pipe(take(1)).subscribe()
+    // We need to handle the formArray now for authors separately
+    // Unfortunately the backend doesn't handle multiple authors yet
+    const firstAuthor = this.form.getRawValue().authors[0] || 'n/a';
+    this.bookApiService.create({ ...this.form.getRawValue(), author: firstAuthor }).pipe(take(1)).subscribe();
+  }
+
+  get authors(): FormArray {
+    return this.form.controls['authors'] as FormArray;
+  }
+
+  deleteAuthor(authorIndex: number) {
+    this.authors.removeAt(authorIndex);
+  }
+
+  addAuthor() {
+    this.authors.push(new FormControl('', [Validators.required, validAuthorName()]));
   }
 }
