499cce3c1e0fff99bca210fd2ea34a8270582cf0
Gregor Woiwode <gregor@entwicklungszeit.dev>
2026-08-28 08:35:12 +0200

    solution--test-testing-library

--- src/app/books/book-card/book-card.spec.ts
+++ src/app/books/book-card/book-card.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { render, screen } from '@testing-library/angular';
 import { BookCard } from './book-card';
 import { Book } from '../book';
 
@@ -10,25 +10,15 @@ describe('BookCard', () => {
     author: 'Herman Melville'
   };
 
-  let component: BookCard;
-  let fixture: ComponentFixture<BookCard>;
+  it('should display the book title', async () => {
+    await render(BookCard, { componentInputs: { book } });
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-
-    fixture = TestBed.createComponent(BookCard);
-    component = fixture.componentInstance;
-    fixture.componentRef.setInput('book', book);
-    fixture.detectChanges();
+    expect(screen.getByText(book.title)).toBeInTheDocument();
   });
 
-  it('should display the book title', () => {
-    const title = fixture.nativeElement.querySelector('h3');
-    expect(title.textContent).toContain('Moby Dick');
-  });
+  it('should display the book author', async () => {
+    await render(BookCard, { componentInputs: { book } });
 
-  it('should display the book author', () => {
-    const author = fixture.nativeElement.querySelector('h4');
-    expect(author.textContent).toContain('Herman Melville');
+    expect(screen.getByText(book.author!)).toBeInTheDocument();
   });
 });
