b2eba8cfc12984430a1c3f38114739a7a3e54c9a
Gregor Woiwode <gregor@entwicklungszeit.dev>
2026-09-06 22:29:29 +0200

    solution--forms-create-book

--- src/app/books/book-create-form/book-create-form.ts
+++ src/app/books/book-create-form/book-create-form.ts
@@ -1,7 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { form, FormField, FormRoot } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-book-create-form',
+  imports: [FormField, FormRoot],
   templateUrl: './book-create-form.html'
 })
-export class BookCreateForm {}
+export class BookCreateForm {
+  protected readonly model = signal({
+    isbn: '',
+    title: '',
+    subtitle: '',
+    author: '',
+    abstract: ''
+  });
+
+  protected readonly form = form(this.model, {
+    submission: {
+      action: formField => {
+        console.log(formField().controlValue(), this.model());
+        return Promise.resolve(null);
+      }
+    }
+  });
+}
