5a330800d186b9c0afcd506826afbbc657f57b02
Gregor Woiwode <gregor@entwicklungszeit.dev>
2026-09-08 12:30:35 +0200

    solution--router-lazy-loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
 import { Welcome } from './lib/shell/welcome/welcome';
-import { BookDetailPage } from './books/book-detail-page/book-detail-page';
-import { BooksPage } from './books/books-page/books-page';
-import { BookCreatePage } from './books/book-create-page/book-create-page';
 
 export const routes: Routes = [
   {
@@ -12,14 +9,6 @@ export const routes: Routes = [
   },
   {
     path: 'books',
-    component: BooksPage
-  },
-  {
-    path: 'books/create',
-    component: BookCreatePage
-  },
-  {
-    path: 'books/detail/:isbn',
-    component: BookDetailPage
+    loadChildren: () => import('./books/book.routes').then(mod => mod.bookRoutes)
   }
 ];
