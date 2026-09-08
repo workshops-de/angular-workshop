a45d6c9db3acaeaa07ffbe03ef557b7e1cd0d50d
Gregor Woiwode <gregor@entwicklungszeit.dev>
2026-09-08 11:39:51 +0200

    solution--component-control-flow

--- src/app/books/book.ts
+++ src/app/books/book.ts
@@ -1,5 +1,5 @@
 export interface Book {
   title: string;
   author: string;
-  publishedAt?: string;
+  publishedAt: Date;
 }
--- src/app/books/books-page/books-page.ts
+++ src/app/books/books-page/books-page.ts
@@ -9,11 +9,24 @@ import { BookCard } from '../book-card/book-card';
   templateUrl: './books-page.html'
 })
 export class BooksPage {
-  book = signal<Book>({
-    title: 'How to win friends',
-    author: 'Dale Carnegie',
-    publishedAt: new Date('1936-10-01')
-  });
+  books = signal<Book[]>([
+    {
+      title: 'How to win friends',
+      author: 'Dale Carnegie',
+      publishedAt: new Date('1936-10-01')
+    },
+    {
+      title:
+        'The Willpower Instinct: How Self-Control Works, Why It Matters, and What You Can Do to Get More of It',
+      author: 'Kelly McGonigal',
+      publishedAt: new Date('2011-12-29')
+    },
+    {
+      author: 'Simon Sinek',
+      title: 'Start with WHY',
+      publishedAt: new Date('2009-10-29')
+    }
+  ]);
 
   goToBookDetails(book: Book) {
     console.log('Navigate to book details, soon...');
