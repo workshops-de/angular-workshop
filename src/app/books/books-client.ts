987727f2214925b86862a6a96bb1b05ab9edc11c
Gregor Woiwode <gregor@entwicklungszeit.dev>
2026-09-08 11:41:32 +0200

    solution--http-query-data

--- src/app/books/books-client.ts
+++ src/app/books/books-client.ts
@@ -1,28 +1,27 @@
-import { Injectable, signal, WritableSignal } from '@angular/core';
+import { httpResource } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Book } from './book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksClient {
-  getAll(): WritableSignal<Book[]> {
-    return signal([
+  readonly #baseUrl = 'http://localhost:4730';
+
+  getAll() {
+    return httpResource<Book[]>(
+      () => ({
+        url: `${this.#baseUrl}/books`,
+        params: {
+          _start: 0,
+          _end: 15,
+          _sort: 'createdAt',
+          _order: 'desc'
+        }
+      }),
       {
-        title: 'How to win friends',
-        author: 'Dale Carnegie',
-        publishedAt: new Date('1936-10-01')
-      },
-      {
-        title:
-          'The Willpower Instinct: How Self-Control Works, Why It Matters, and What You Can Do to Get More of It',
-        author: 'Kelly McGonigal',
-        publishedAt: new Date('2011-12-29')
-      },
-      {
-        author: 'Simon Sinek',
-        title: 'Start with WHY',
-        publishedAt: new Date('2009-10-29')
+        defaultValue: []
       }
-    ]);
+    );
   }
 }
