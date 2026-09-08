7890ec61e33e691e4f69d39011e7512e7c131c14
Gregor Woiwode <gregor@entwicklungszeit.dev>
2026-09-08 11:39:28 +0200

    solution--component-output-binding

--- src/app/books/books-page/books-page.ts
+++ src/app/books/books-page/books-page.ts
@@ -14,4 +14,14 @@ export class BooksPage {
     author: 'Dale Carnegie',
     publishedAt: new Date('1936-10-01')
   });
+
+  goToBookDetails(book: Book) {
+    console.log('Navigate to book details, soon...');
+    console.table(book);
+  }
+
+  deleteBook(book: Book) {
+    console.log('Delete book, soon...');
+    console.table(book);
+  }
 }
