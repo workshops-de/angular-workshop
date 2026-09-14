c3c7db94ac21c34941a64b897fdda62e07fe0a6b
Gregor Woiwode <gregor@entwicklungszeit.dev>
2026-09-14 13:18:54 +0200

    solution--component-output-binding

--- src/app/books/book-card/book-card.ts
+++ src/app/books/book-card/book-card.ts
@@ -1,4 +1,4 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { Book } from '../book';
 
 @Component({
@@ -11,10 +11,22 @@ export class BookCard {
   readonly placeholderCover = 'book-cover-placeholder.svg';
 
   readonly book = input.required<Book>();
+  readonly detailClick = output<Book>();
+  readonly deleteClick = output<Book>();
 
   handleDetailClick(click: MouseEvent) {
     click.preventDefault();
 
     console.log('Click Details-Link:', click);
+
+    this.detailClick.emit(this.book());
+  }
+
+  handleDeleteClick(click: MouseEvent) {
+    click.preventDefault();
+
+    console.log('Click Delete-Button:', click);
+
+    this.deleteClick.emit(this.book());
   }
 }
